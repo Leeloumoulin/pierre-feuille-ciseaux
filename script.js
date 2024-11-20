4c0b7ad871f32bd84d39c60eaa3524d4c859cda6
Leelou <leeloumoulin@gmail.com>
2024-11-20 17:24:35 +0100

    debut

---
+++ script.js
@@ -0,0 +1,6 @@
+// créer 3 variables contenant pierre papier et ciseaux et 2 variables joueurs : PC et humain
+// prompt pour choisir ce qu'on joue
+// L'ordinateur choisi à son tour : math.floor et math.random 
+// les conditions 
+// Afficher le résultat 
+
